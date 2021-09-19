cb862536abf6828be35a6ad6716d63b3ac8a1204
Alex <globophobe@gmail.com>
2021-09-19 22:34:34 +0900

    first commit

---
+++ layouts/cover.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="slidev-layout cover h-full grid place-content-center">
+    <div class="my-auto w-full text-center">
+      <slot />
+    </div>
+  </div>
+</template>
---
+++ layouts/default.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="slidev-layout default center h-full grid place-content-center">
+    <div class="my-auto">
+      <slot />
+    </div>
+  </div>
+</template>
